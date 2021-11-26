615c2f70e4b34e71dc768bf460afa0dde9f3eaa2
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-11-26 18:59:14 +0530

    delete string in trie

--- Trie.py
+++ Trie.py
@@ -32,10 +32,47 @@ class Trie:
             return True
         return False
 
+def deleteString(root,word,index):
+    ch=word[index]
+    current=root.children.get(ch)
+    canDelete=False
+    
+    #At the last node
+    if index==len(word)-1:
+        if len(current.children)>=1:
+            current.endOfString=False
+            return False
+        else:
+            root.children.pop(ch)
+            return True
+
+    # if node's children more than 1, then we can't del the node
+    if len(current.children)>1:
+        # print("more than 1 child: "+ch)
+        deleteString(current,word,index+1)
+        return False
+
+    #This is used in case of backtracking, if we are not at last index and get endOfString
+    # we cannot delete from here eg (APIS,AP) delete APIS
+    if current.endOfString==True:
+        deleteString(current,word,index+1)
+        return False
+
+    canDelete=deleteString(current,word,index+1)
+    if canDelete==True:
+        root.children.pop(ch)
+        return True
+    return False
+
+
 
 trie=Trie()
 trie.insertString("App")
-trie.insertString("Appl")
-trie.insertString("B")
+# trie.insertString("Appl")
+# #trie.insertString("Appr")
+# trie.insertString("AB")
+# trie.insertString("Apr")
 print(trie.root.children)
+print(deleteString(trie.root,"App",0))
 print(trie.searchString("App"))
+print(trie.root.children)
